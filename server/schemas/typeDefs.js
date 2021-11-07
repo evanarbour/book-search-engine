0cfdd2df7a79650f6f6d897a2b7fc2ff40723ac5
Evan Arbour <evan.arbour@gmail.com>
2021-11-07 14:51:02 -0800

    created typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,48 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql `
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: ID
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: String
+        user: User
+    }
+
+    input BookInput {
+        bookId: String
+        authors: [String]
+        description: String
+        image: String
+        link: String
+        title: String
+    }
+
+    type Query {
+        me: [User]
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(BookData: BookInput!): User
+        removeBook(bookId: ID!): User
+
+    }
+`;
+module.exports = typeDefs;
