5ae48e37aded8f48c52e5771067450bdf8c36b51
Evan Arbour <evan.arbour@gmail.com>
2021-11-09 09:52:43 -0800

    edited signupform.js to include add_user mutation

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,7 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
+import React from 'react';
+import { Jumbotron, 
+        Container, 
+        CardColumns, 
+        Card, 
+        Button } from 'react-bootstrap';
 
-// import { getMe, deleteBook } from '../utils/API';
+
 import { GET_ME } from '../utils/queries';
 import { REMOVE_BOOK } from '../utils/mutations';
 import Auth from '../utils/auth';
